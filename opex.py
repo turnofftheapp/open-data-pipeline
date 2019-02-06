1ad0954925c255c33d0279e8b1d4bf83150af3c4
Jack <jackstephenson96@gmail.com>
2019-02-06 13:22:04 -0500

    working on get_endpoints

--- opex.py
+++ opex.py
@@ -3,6 +3,9 @@
 ###### Version 1 ######
 
 import requests
+import json
+# from itertools import ifilter
+
 
 
 '''
@@ -12,13 +15,32 @@ Resources:
 - Overpass Turbo Workbench []
 '''
 
-import requests
+'''
+TODO:
+[] discuss best data structure to use (for recursively finding start and end nodes in relations, later injecting bus stop info to nodes)
+'''
 
+
+## Query for one relation: "North Country Trail"
 q = '[out:json][timeout:25];relation["route"="hiking"](46.561516046166,-87.437782287598,46.582255876979,-87.39284992218);(._;>;);out;'
 ql = {'data':q}
-michigan = requests.get('https://overpass-api.de/api/interpreter', params=ql)
+nct = requests.get('https://overpass-api.de/api/interpreter', params=ql)
 
-print(michigan.text)
+geoJlist = json.loads(nct.text)['elements']
+relations = [element for element in geoJlist if element['type'] == "relation"]
+ways = [element for element in geoJlist if element['type'] == "way"]
+nodes = [element for element in geoJlist if element['type'] == "node"]
+
+
+
+def get_endpoints(geoJlist):
+	for rel in relations: 
+		relFirstWayID = rel['members'][0]['ref']
+		relLastWayID = rel['members'][-1]['ref']
+		print(relFirstWayID, relLastWayID)
+
+
+
 
 
 
