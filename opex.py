9987f551b7ebadbede40dcac3ed431681c4bfc8e
Jack <jackstephenson96@gmail.com>
2019-02-07 15:16:35 -0500

    added caching feature, separated into opex script and util file for helper functions

--- opex.py
+++ opex.py
@@ -3,45 +3,63 @@
 ###### Version 1 ######
 import pandas as pd
 import requests
+import requests_cache
 import json
+import os
+import util
+
+requests_cache.install_cache('demo_cache')
+
 
 pd.set_option('display.max_colwidth', -1)
-
-# Whole dataset for Mich
-q = '[out:json][timeout:25];area(3600165789)->.searchArea;relation["route"="hiking"](area.searchArea);(._;>;);out;'
-ql = {'data':q}
-mich = requests.get('https://overpass-api.de/api/interpreter', params=ql)
 
 # dataset for 1 trail
 # q = '[out:json][timeout:25];relation["route"="hiking"](46.561516046166,-87.437782287598,46.582255876979,-87.39284992218);(._;>;);out;'
 # ql = {'data':q}
 # mich = requests.get('https://overpass-api.de/api/interpreter', params=ql)
 
-geoJlist = json.loads(mich.text)['elements']
+# Gets whole dataset for state of Michigan
+def getturbojson(area="MI"):
+	
+	# TODO: implement in util
+	# areacode = util.getArea(area)
+	areacode = '3600165789'
+	query = '[out:json][timeout:25];area({})->.searchArea;relation["route"="hiking"](area.searchArea);(._;>;);out;'.format(areacode)
+	pckg = {'data':query}
+	outs = requests.get('https://overpass-api.de/api/interpreter', params=pckg)
+	geoJelements = json.loads(outs.text)['elements']
 
-nodes = [element for element in geoJlist if element['type'] == "node"]
-ways = [element for element in geoJlist if element['type'] == "way"]
-relations = [element for element in geoJlist if element['type'] == "relation"]
+	# Filter objects by type
+	for element in geoJelements:
+		returnDict = {}
+		# print(element)
+		nodes, ways, relations, other = [], [], [], []
+		if element['type'] == "node":
+			nodes.append(element)
+		elif element['type'] == "way":
+			nodes.append(element)
+		elif element['type'] == "relation":
+			nodes.append(element)
+		else:
+			other.append(element)
 
-rel = pd.DataFrame(relations)
-wa = pd.DataFrame(ways)
-nod = pd.DataFrame(nodes)
+	# Enter into dataframes
+	rel_df = pd.DataFrame(relations)
+	way_df = pd.DataFrame(ways)
+	nod_df = pd.DataFrame(nodes)
 
-def get_coords(c):
-    firstway_id = c['members'][0]['ref']
-    firstnode_id = list(wa.loc[wa['id'] == firstway_id]['nodes'])[0][0]
-    firstnode_coords = nod.loc[nod['id'] == firstnode_id]
-    begin_lat = float(firstnode_coords['lat'])
-    begin_lon = float(firstnode_coords['lon'])
-    
-    lastway_id = c['members'][-1]['ref']
-    lastnode_id = list(wa.loc[wa['id'] == lastway_id]['nodes'])[0][-1]
-    lastnode_coords = nod.loc[nod['id']==lastnode_id]
-    end_lat = float(lastnode_coords['lat'])
-    end_lon = float(lastnode_coords['lon'])
-    return (begin_lat, begin_lon, end_lat, end_lon)
-    
-rel['start_way'] = rel.apply(get_coords, axis=1)
+	returnDict['rel_df'] = rel_df
+	returnDict['way_df'] = way_df	
+	returnDict['nod_df'] = nod_df
+
+	return returnDict
+	# return rel_df
+
+elementDict = getturbojson()
+print(elementDict['nod_df'].head())
+
+	
+# rel['start_way'] = rel.apply(get_coords, axis=1)
 # # rel['end_way'] = rel.apply(get_end_node, axis = 1)
 
 '''
@@ -49,6 +67,13 @@ rel['start_way'] = rel.apply(get_coords, axis=1)
 2. for first way ID, get first node , for last way ID get last node
 3. profit
 '''
+
+# class Trail(self, 
+# 			id=id_int, 
+# 			lat=lat_int, 
+# 			lon=lon_int 
+# 			tags=tags, 
+# 			type=type):
 
 
 
---
+++ util.py
@@ -0,0 +1,29 @@
+#### UMSIvsTOTAGO  ####
+## Utility Functions ##
+###### Version 1 ######
+import pandas as pd
+import requests
+import json
+import os
+
+
+## IN: relation
+## OUT: begin_lat int, begin_lon int, end_lat int, end_lon int
+def get_coords(c):
+
+	firstway_id = c['members'][0]['ref']
+	print(firstway_id)
+
+	# firstnode_id = list(wa.loc[wa['id'] == firstway_id]['nodes'])[0][0]
+	# firstnode_coords = nod.loc[nod['id'] == firstnode_id]
+	# begin_lat = float(firstnode_coords['lat'])
+	# begin_lon = float(firstnode_coords['lon'])
+	
+	lastway_id = c['members'][-1]['ref']
+	print(lastway_id)
+
+	# lastnode_id = list(wa.loc[wa['id'] == lastway_id]['nodes'])[0][-1]
+	# lastnode_coords = nod.loc[nod['id']==lastnode_id]
+	# end_lat = float(lastnode_coords['lat'])
+	# end_lon = float(lastnode_coords['lon'])
+	return (begin_lat, begin_lon, end_lat, end_lon)
