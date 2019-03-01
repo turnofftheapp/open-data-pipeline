b0674fd2a01b27b440ddf84877280c53c61c4420
Jack <jackstephenson96@gmail.com>
2019-02-28 23:26:49 -0500

    added getStateAreaId() helper, can now query different states

--- util.py
+++ util.py
@@ -10,14 +10,13 @@ import os
 
 ## IN: country, region
 ## OUT: Overpass region code
-def getArea(country='US', region='MI'):
-	'''
-	Make request to overpass or wherever to retreive geo encodings.
-	In the future we'll use this function to make it easier to 
-	switch states and areas when querying.
-	'''
-	pass
-
+def getStateAreaId(state_full_name):
+	states = {}
+	state = state_full_name.lower()
+	with open("us-state-relations.csv") as f:
+		for line in f.readlines()[1:]:
+			states[line.split(',')[1].lower()] = int(line.split(',')[2])
+	return states[state]
 
 ## To be applied to df
 ## IN: row iterator object (c)
@@ -30,6 +29,7 @@ def getBus(c):
 ## IN: row iterator object (c)
 ## OUT: new col w int elevation_gain
 ''' ISSUE: we still need to clarify how we determine "trail start" and "trail end"'''
+
 def getElevation(c):
 	base_url = "https://maps.googleapis.com/maps/api/elevation/json?locations="
 	gKey = "AIzaSyChXIQQQrzdfeuPPFgY_RQKSQZgvXdwTV8"
@@ -50,6 +50,14 @@ def getElevation(c):
 		e = e1-e2
 		e_change.append(e)
 	return e_change
+
+def getRoundTripDist(c):
+	'''For each relation: need to eval if trail is a loop or not, then figure out how to calc dist'''
+	pass
+
+
+
+
 
 
 	# DB Schema: https://docs.google.com/document/d/1D_bjp7f0lv7hRCPbL2rCDwIlX152Pmr9M81Dwwt-iQk/edit
