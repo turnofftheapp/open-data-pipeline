8820546d1d1678f7c3756fb74dbc7f71b8db2690
Jack <jackstephenson96@gmail.com>
2019-03-26 16:01:28 -0400

    added polyline column for each trail

--- geoPipe.py
+++ geoPipe.py
@@ -16,38 +16,26 @@ import util
 DEBUG_MODE = False
 STATE = "Michigan"
 # WAYLIMIT = 100
-requests_cache.install_cache('demo_cache')
+# requests_cache.install_cache('demo_cache')
 
 pd.set_option('display.max_colwidth', -1)
 pd.set_option('display.max_columns', 100)
-
-# dataset for 1 trail
-# q = '[out:json][timeout:25];relation["route"="hiking"](46.561516046166,-87.437782287598,46.582255876979,-87.39284992218);(._;>;);out;'
-# ql = {'data':q}
-# mich = requests.get('https://overpass-api.de/api/interpreter', params=ql)
+pd.set_option('display.max_rows', 100000)
 
 ## IN: nothing, yet, will add parameters
 ## Out: nodeID, begin_lat, begin_lon, end_lat, end_lon
 def queryToDf(state):
 
-	area = util.getStateAreaId(state)
-	query = '[out:json][timeout:25]; area({0})->.searchArea; (way["highway"~"path|footway|cycleway|bridleway"]\
-	["name"~"trail|Trail|Hiking|hiking"](area.searchArea);<;);(._;>;);out;'.format(area)
-	# query = '[out:json][timeout:25];area(3600165789)->.searchArea;relation["route"="hiking"](area.searchArea);(._;>;);out;'
-	# query = '[out:json][timeout:25];relation["route"="hiking"](46.561516046166,-87.437782287598,46.582255876979,-87.39284992218);(._;>;);out;'
-	pckg = {'data':query}
-	r = requests.get('https://overpass-api.de/api/interpreter', params=pckg, stream=True)
-	# Total size in bytes.
-	total_size = int(r.headers.get('content-length', 0)); 
-	block_size = 1024
-	wrote = 0 
+	area = util.get_state_area_id(state)
 
-	with open('Output/queryoutput.bin', 'wb') as f:
-		for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size//block_size) , unit='KB', unit_scale=True):
-			wrote = wrote  + len(data)
-			f.write(data)
-	# if total_size != 0 and wrote != total_size:
-	# 	print("ERROR, something went wrong")  
+	# commented queries are smaller and used for development only
+	# query = '[out:json][timeout:25]; area({0})->.searchArea; (way["highway"~"path|footway|cycleway|bridleway"]\
+	# ["name"~"trail|Trail|Hiking|hiking"](area.searchArea);<;);(._;>;);out;'.format(area)
+	query = '[out:json][timeout:25];area(3600165789)->.searchArea;relation["route"="hiking"](area.searchArea);(._;>;);out;'
+	# query = '[out:json][timeout:25];relation["route"="hiking"](46.561516046166,-87.437782287598,46.582255876979,-87.39284992218);(._;>;);out;'
+	
+	pckg = {'data':query}
+	r = requests.get('https://overpass-api.de/api/interpreter', params=pckg)
 
 	geoJ = json.loads(r.text)
 	geoJelements = geoJ['elements']
@@ -63,6 +51,7 @@ def transform_members(c, way_df, nod_df):
 	ways = []
 	nodes = []
 	errors = []
+
 	# To find the type of each member, we first need to know what key is used (sometimes 'role' is used instead of 'type')
 	for way in c['members']:
 		if way['type'] != '':
@@ -70,35 +59,31 @@ def transform_members(c, way_df, nod_df):
 		elif way['role'] != '':
 			way_obj = way['role']
 
-	# Now, catch bad data where nodes are included in members instead of ways
-	if way_obj == "node":
-		errors.append((c['id'], way))
+		# Now, catch bad data where nodes are included in members instead of ways
+		if way_obj == "node":
+			errors.append((c['id'], way))
 
-	# If the object is a way, 
-	elif way_obj == "way":
-		try:
-			w = way_df.loc[way_df['id'] == way['ref']]
-			way_id = int(w['id'])
-			way_tags = list(dict(w['tags']).values())
-			obj_type = str(w['type'])
-			try: 
-				tags = list(dict(w['tags']).values())[0]
-			except Exception:
-				#this only happens if there aren't tags to begin with
-				tags = []
-			way = {'id':way_id, 'tags':tags, 'role':obj_type}
-			ways.append(way)
+		# If the object is a way, 
+		elif way_obj == "way":
+			try:
+				w = way_df.loc[way_df['id'] == way['ref']]
+				way_id = int(w['id'])
+				way_tags = list(dict(w['tags']).values())
+				obj_type = str(w['type'])
+				try: 
+					tags = list(dict(w['tags']).values())[0]
+				except Exception:
+					#this only happens if there aren't tags to begin with
+					tags = []
+				way = {'id':way_id, 'tags':tags, 'role':obj_type}
+				ways.append(way)
 
-			for node in list(w['nodes'])[0]:
-				n = nod_df.loc[nod_df['id'] == node]
-				node = {'id':int(n['id']), 'lat':float(n['lat']), 'lon':float(n['lon'])}
-				nodes.append(node)
-		except Exception as e:
-			errors.append((e, c))
-			# print("way error: ", e, " on: \n")
-			# print(c)
-			# print("*************************")
-
+				for node in list(w['nodes'])[0]:
+					n = nod_df.loc[nod_df['id'] == node]
+					node = {'id':int(n['id']), 'lat':float(n['lat']), 'lon':float(n['lon'])}
+					nodes.append(node)
+			except Exception as e:
+				errors.append((e, c))
 
 	c['ways'] = ways
 	c['nodes'] = nodes
@@ -119,10 +104,13 @@ def main():
 
 	# 1. query OSM and send results to df
 	geoJelements_df = queryToDf(STATE)
-
+	# with open('raw.txt', "w") as f:
+	# 	f.write(str(geoJelements_df))
 	# 2. split into 3 dfs by type 
+
 	print("processing response...")
 	dfs = [x for _, x in geoJelements_df.groupby('type')]
+
 	nod_df = dfs[0]
 	rel_df = dfs[1]
 	way_df = dfs[2]
@@ -144,7 +132,7 @@ def main():
 	trail_df = trail_df.progress_apply(util.get_name, axis=1)
 	# print(trail_df)
 
-	# 6. Determind shape: oop or out-and-back, get end lat and lon if loop
+	# 6. Determind shape: loop or out-and-back, get end lat and lon if loop
 	trail_df = trail_df.apply(util.get_shape, axis=1)
 	trail_df = trail_df.apply(util.get_trail_end, axis=1)
 
@@ -154,6 +142,12 @@ def main():
 	'''
 	print("removing false positives")
 	trail_df = trail_df[trail_df['issues'].map(len) == 0]
+
+	print("encoding polyline")
+	trail_df = trail_df.progress_apply(util.get_polyline, axis=1)
+
+
+	print(trail_df.columns)
 	print(trail_df)
 
 
--- util.py
+++ util.py
@@ -5,10 +5,11 @@ import pandas as pd
 import requests
 import json
 import os
+import polyline
 
 ## IN: country, region
 ## OUT: Overpass region code
-def getStateAreaId(state_full_name):
+def get_state_area_id(state_full_name):
 	states = {}
 	state = state_full_name.lower()
 	with open("us-state-relations.csv") as f:
@@ -21,37 +22,6 @@ def getStateAreaId(state_full_name):
 ## OUT: new column containing presence and location of bus stop, we will use this to designate a trailhead
 def getBus(c):
 	## for Sam; just implement what you did in jupyter here, then apply it in geoPipe's main method
-	pass
-
-## To be applied to df
-## IN: row iterator object (c)
-## OUT: new col w int elevation_gain
-''' ISSUE: we still need to clarify how we determine "trail start" and "trail end"'''
-def getElevation(c):
-	base_url = "https://maps.googleapis.com/maps/api/elevation/json?locations="
-	gKey = "AIzaSyChXIQQQrzdfeuPPFgY_RQKSQZgvXdwTV8"
-	coords = []
-	e_change = []
-	error = []
-	for way in c['members']:
-		try:
-			w = list(way_df.loc[way_df['id'] == way['ref']]['nodes'])
-			fnode = nod_df.loc[nod_df['id']==w[0][0]]
-			lnode = nod_df.loc[nod_df['id']==w[0][-1]]
-			coords.append((str(fnode['lat'])+","+str(fnode['lon']), str(lnode['lat'])+","+str(lnode['lon'])))
-		except Exception:
-			error.append(way)
-	for coord in coords:
-		e1 = base_url + coord[1] + "&key=" + gKey
-		e2 = base_url + coord[2] + "&key=" + gKey
-		e = e1-e2
-		e_change.append(e)
-	return e_change
-
-def getRoundTripDist(c):
-	'''use info from shape column,
-	   could use google libraries
-	   once we geocode'''
 	pass
 
 ## To be applied to df
@@ -99,7 +69,7 @@ def get_shape(c):
 			if fnode == lnode:
 				c['shape'] = "loop"
 			else:
-				c['shape'] = "out and back"
+				c['shape'] = "straight"
 	except Exception as e:
 		print("get_shape error ", e, " on trail id: ", c.id)
 	return c
@@ -110,17 +80,17 @@ def get_shape(c):
 def get_trail_end(c):
 	c['end_lat'] = None
 	c['end_lon'] = None
-	if c['shape'] == "loop":
+	if c['shape'] == "straight":
 		c['end_lat'] = float(c['nodes'][-1]['lat'])
 		c['end_lon'] = float(c['nodes'][-1]['lon'])
 	return c
 
-## IN: dataframe, id of row to remove
-## OUT: dataframe without removed row
-def remove_trail(df, idlist):
-
-	pass
-
+def get_polyline(c, precision=5):
+	nodes = []
+	for node in c['nodes']:
+		nodes.append((float(node['lat']), float(node['lon'])))
+	c['polyline'] = polyline.encode(nodes, precision)
+	return c
 
 
 	# DB Schema: https://docs.google.com/document/d/1D_bjp7f0lv7hRCPbL2rCDwIlX152Pmr9M81Dwwt-iQk/edit
